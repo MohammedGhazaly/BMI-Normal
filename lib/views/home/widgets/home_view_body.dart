925057f5b58e749af2d32ab2a773b88ee33850e7
MohammedGhazaly <warendragon67@gmail.com>
2023-12-02 17:50:05 +0200

    Finished slider widget

--- lib/views/home/widgets/home_view_body.dart
+++ lib/views/home/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_normal/constants.dart';
 import 'package:bmi_normal/views/home/widgets/gender_container.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +8,10 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
               height: 16,
@@ -26,15 +28,57 @@ class HomeViewBody extends StatelessWidget {
                   width: 8,
                 ),
                 Expanded(
-                    child: GenderContainer(
-                  isSelected: false,
-                  gender: "Female",
-                  icon: Icons.female_rounded,
-                )),
+                  child: GenderContainer(
+                    isSelected: false,
+                    gender: "Female",
+                    icon: Icons.female_rounded,
+                  ),
+                ),
               ],
-            )
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomSliderWidget()
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CustomSliderWidget extends StatelessWidget {
+  const CustomSliderWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+        color: kBlueColor,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Height",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          Text(
+            "300",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          Slider(
+            min: 0,
+            max: 300,
+            value: 150,
+            activeColor: kRedColor,
+            onChanged: (value) {},
+          ),
+        ],
       ),
     );
   }
