a32c66a713cd66072e2ae679b7f211ecd3e1b54b
MohammedGhazaly <warendragon67@gmail.com>
2023-12-02 12:17:44 +0200

    Finished App bar

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color kBlueColor = const Color(0xff87b1e3);
+Color kRedColor = const Color(0xfff48c8c);
+Color kLightColor = const Color(0xfffafafa);
---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,22 @@
+import 'package:bmi_normal/constants.dart';
+import 'package:bmi_normal/views/home/widgets/home_view_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  static String routeName = "home-view";
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text("BMI Calculator"),
+        backgroundColor: Colors.white,
+        foregroundColor: kBlueColor,
+      ),
+      body: HomeViewBody(),
+    );
+  }
+}
